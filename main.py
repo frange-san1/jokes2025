91c31e32dddbb6b939af641fa5f55c79af935ee9
Rangel-Santos <frangel-san1@cps.k12.il.us>
2025-01-17 09:34:00 -0600

    eveyrthimng

--- main.py
+++ main.py
@@ -155,35 +155,39 @@ while joke == "yes":
     print("Great, Let's Play")
     question = input("Do you want to hear a joke about robbers, tanks, or pencils? ") # Gives the User 3 joke options to choose from
     question = question.lower() # Makes "question" input lowercase, which allows for all inputs to be recognized no matter the capitilization.
-    for jokes in joke_list:
-        if question == "robbers": # Joke Option #1.
+    if question == "robbers": # Joke Option #1.
             input("Knock Knock ")
             input("Calder ")
             print("Calder police - I've been robbed!")
             joke = input("Do you want to hear another joke or are you finished? ")  # Asks the User if they'd like to listen to another joke
         
-        elif question == "tanks": # Joke Option #2.
+    elif question == "tanks": # Joke Option #2.
             input("Knock Knock ") 
             input("Tank ")
             print("You are welcome!")
             joke = input("Do you want to hear another joke or are you finished? ")  # Asks the User if they'd like to listen to another joke
 
-        elif question == "pencils": # Joke Option #3.
+    elif question == "pencils": # Joke Option #3.
             input("Knock Knock ")
             input("Broken pencil ")
             print("Nevermind, it's pointless!")
             joke = input("Do you want to hear another joke or are you finished? ")  # Asks the User if they'd like to listen to another joke
 
 
-scoring = [] # Purpose: Empty List for the User's score of the game, which allows the scoring process to have a result.
-if joke == "finished":
-    rate = int(input("Please rate our game 1-10! ")) # Asks for User's rating.
-    scoring = rate # Inputs rate into empty list, so it can go through scoring process.
     def scoring_process(scoring): # Function for scoring process.
-        final_score = int(scoring * 10) # Uses scoring list and multiplies it by last to get final score percentage
+        final_score = [] # Purpose: Empty List for the User's score of the game, which allows the scoring process to have a result.
+
+        for score in scoring:
+            if joke == "finished":
+               score = int((scoring * 10)/10000000000) 
+               final_score = score
+               
         return final_score
-    result = str(scoring_process(scoring))
-    print((result) + " percent satisfaction rate")# Prints out function result.
+    
+    scoring = input("Please rate our game 1-10! ") # Asks for User's rating.
+    result = scoring_process(scoring)
+    print((result), " percent satisfaction rate") # Prints out function result.
+
     if int(result) > 70:
         friend = input("Glad to hear you enjoyed the game! Would you recommend this game to a friend? ") # Asks User if they would recommend this game.
     else:
